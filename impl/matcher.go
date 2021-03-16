9e21cb3feb2e40f7792fa89da6db39b3a23c7040
Mikkel Juul <mju@dmi.dk>
2021-03-16 13:19:14 +0100

    matcher.go doc

--- impl/matcher.go
+++ impl/matcher.go
@@ -2,6 +2,8 @@ package impl
 
 import "regexp"
 
+//Matcher is simply an interface that regexp.Regexp fulfill (with the method we care for)
+//This way we can implement another Matcher fulfilling this interface
 type Matcher interface {
 	Match([]byte) bool
 }
@@ -11,6 +13,7 @@ func (matcher MatcherFunc) Match(b []byte) bool {
 	return matcher(b)
 }
 
+//NewMatcher returns a matcher that is the regexp.Regexp or a function that is always true
 func NewMatcher(pattern string) (Matcher, error) {
 	if pattern != "" {
 		return regexp.Compile(pattern)
