5819d9ab261f9836e4163b549176eed544f314c0
Mikkel Juul <mju@dmi.dk>
2021-04-07 22:05:38 +0200

    remove composite client

--- docker_build.bash
+++ docker_build.bash
@@ -6,14 +6,11 @@ client_tag="${2:?no version tag given for client}"
 docker build -t base -f dockerfiles/Dockerfile_base .
 docker build -t ld -f dockerfiles/Dockerfile_ld .
 docker build --build-arg VERSION="$client_tag" -t ld-client -f dockerfiles/Dockerfile_client .
-docker build -t ldwclient -f dockerfiles/Dockerfile_ldwclient .
 
 docker tag ld mjuul/ld:"$tag"
 docker tag ld-client mjuul/ld-client:"$client_tag"
 docker tag ld-client mjuul/ld-client:latest
-docker tag ldwclient mjuul/ld:"$tag"-client
 
 docker push mjuul/ld:"$tag"
 docker push mjuul/ld-client:"$client_tag"
 docker push mjuul/ld-client:latest
-docker push mjuul/ld:"$tag"-client
