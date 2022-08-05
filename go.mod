b19b15505aeb074f4b878561c1605152a54c43f3
Mikkel Juul <mjuuldev@gmail.com>
2022-08-05 19:55:11 +0200

    next increment

--- go.mod
+++ go.mod
@@ -1,9 +1,20 @@
 module github.com/MikkelHJuul/ld
 
-go 1.16
+go 1.19
 
 require (
+	github.com/bufbuild/connect-go v0.1.1
 	github.com/sirupsen/logrus v1.8.1
 	google.golang.org/grpc v1.46.0
 	google.golang.org/protobuf v1.28.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	go.etcd.io/bbolt v1.3.6
+	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
+	golang.org/x/sync v0.0.0-20220601150217-0de741cfad7f
+	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
+	golang.org/x/text v0.3.3 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
 )
--- main.go
+++ main.go
@@ -1,20 +1,16 @@
 package main
 
 import (
-	"errors"
 	"flag"
-	"net"
+	"net/http"
 	"os"
-
-	log "github.com/sirupsen/logrus"
-	"google.golang.org/grpc"
-	_ "google.golang.org/grpc/encoding/gzip"
 )
 
 var (
 	port       = flag.String("port", lookupEnvOrString("PORT", "5326"), "The server port, default 5326")
 	dbLocation = flag.String("db-location", lookupEnvOrString("DB_LOCATION", "ld_db"), "folder location where the database is situated")
-	logLevel   = flag.String("log-level", lookupEnvOrString("LOG_LEVEL", "INFO"), "configure logging level")
+
+//	logLevel   = flag.String("log-level", lookupEnvOrString("LOG_LEVEL", "INFO"), "configure logging level")
 )
 
 func lookupEnvOrString(key string, defaultVal string) string {
@@ -26,35 +22,15 @@ func lookupEnvOrString(key string, defaultVal string) string {
 
 func main() {
 	flag.Parse()
-	loggingLevel, err := log.ParseLevel(*logLevel)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.SetLevel(loggingLevel)
-	lis, err := net.Listen("tcp", "localhost:"+*port)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	_, err = os.Stat(*dbLocation)
-	if err != nil {
-		if errors.Is(err, os.ErrNotExist) {
-			log.Fatalf(`could not open file, does not exist:`, *dbLocation)
-		} else {
-			log.Fatalf(`unexpected error with database location:`, *dbLocation)
-		}
-	}
-	var opts []grpc.ServerOption
-	var l = ldServer{make(map[string]*service)}
-	opts = append(opts, grpc.UnknownServiceHandler(l.serveUnknownService()))
-	// create general server
-	// use grpc.UnkownServiceHandler to service requests (it's hacky! but it works)
-	// link this and admin server somehow (how tight should the bound be?)
+	//	loggingLevel, err := log.ParseLevel(*logLevel)
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
+	//	log.SetLevel(loggingLevel)
+	var handleRequests chan RoutingRequest = make(chan RoutingRequest)
+	mux := NewInternalLdMux(handleRequests)
 
-	grpcServer := grpc.NewServer(opts...) // unkown server is injected via options
+	//orchestrate the admin page and Server...
 
-	// create and register admin server
-
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("server exited with error: %v", err)
-	}
+	_ = &http.Server{Addr: "localhost:8080", Handler: mux}
 }
